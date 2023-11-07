0b1dea89c56c09342328efc8d1ce319c9639091d
Amy <ap.amruta59@gmail.com>
2023-11-07 18:26:12 +0530

    file template

--- template.py
+++ template.py
@@ -1,21 +1,21 @@
 import os
 from pathlib import Path
 
-package_name = "DiamondPricePrediction"\
+package_name="DimondPricePrediction"
 
-list_of_files = [
+list_of_files=[
     "github/workflows/.gitkeep",
-    f'src/{package_name}/__init__.py',
-    f'src/{package_name}/components/__init__.py',
-    f'src/{package_name}/components/data_ingetion.py',
-    f'src/{package_name}/components/data_transformation.py',
-    f'src/{package_name}/components/model_trainer.py',
-    f'src/{package_name}/pipelines/__init__.py',
-    f'src/{package_name}/pipelines/training_pipeline.py',
-    f'src/{package_name}/pipelines/prediction_pipeline.py',
-    f'src/{package_name}/logger.py',
-    f'src/{package_name}/exception.py',
-    f'src/{package_name}/utils/__init__.py'
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/components/data_ingestion.py",
+    f"src/{package_name}/components/data_transformation.py",
+    f"src/{package_name}/components/model_trainer.py",
+    f"src/{package_name}/pipelines/__init__.py",
+    f"src/{package_name}/pipelines/training_pipeline.py",
+    f"src/{package_name}/pipelines/prediction_pipeline.py",
+    f"src/{package_name}/logger.py",
+    f"src/{package_name}/exception.py",
+    f"src/{package_name}/utils/__init__.py",
     "notebooks/research.ipynb",
     "notebooks/data/.gitkeep",
     "requirements.txt",
@@ -23,26 +23,25 @@ list_of_files = [
     "init_setup.sh",
 ]
 
-#Here We'll Create a Directory
+
+# here will create a directory
 
 for filepath in list_of_files:
     filepath=Path(filepath)
-    filedir, filename = os.path.split(filepath)
-
+    filedir,filename=os.path.split(filepath)
+    
+    """ how exist_ok works:if "directory" already exists, 
+    os.makedirs() will not raise an error, and it will do nothing. 
+    If "my_directory" doesn't exist, it will create the directory.
     """
-    How exist_ok works: if 'directory' already exists,
-    os.makedirs() will not raise an error, and it will do nothing.
-
-    If 'my_directory' doesn't exist, it will create the directory.
-    """
-
-    if filedir!="":
+    if filedir != "":
         os.makedirs(filedir,exist_ok=True)
-
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath,'w') as f :
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
             pass
     else:
-        print('file already exists')
+        print("file already exists")
 
-        #Here we will use the file handling.
+# here will use the file handling
+
